53856c74f92ebe93040ad03ab5c3e9a75c9a86d5
Kworker <mixteamlp@gmail.com>
2020-02-07 16:17:51 +1000

    bug fix
    Signed-off-by: Kworker <mixteamlp@gmail.com>

--- MySecretaryMaven/MySecretary/CreateDataBase.sql
+++ MySecretaryMaven/MySecretary/CreateDataBase.sql
@@ -1,7 +1,7 @@
 create SCHEMA `mysecretary` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-create TABLE `mysecretary`.`bot_users` (
+create TABLE `mysecretary`.`bot_users`(
   `email` VARCHAR(30) NULL,
-  `accessToken` VARCHAR(45) NOT NULL,
+  `accessToken` VARCHAR(100) NOT NULL,
   `id` INT NOT NULL,
   `reply` VARCHAR(300) NOT NULL,
   PRIMARY KEY (`id`));
